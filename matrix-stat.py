691d9bdfaa1c0652c4c4cf905b5c7f02eae12135
Chris Orchard <cso1g09@ecs.soton.ac.uk>
2013-05-14 21:44:01 +0100

    Updated script to get statistics from ipv6matrix

--- matrix-stat.py
+++ matrix-stat.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python
 
 import sqlite3
+import sys
+import os
 
+dbpaths = []
 
-
-if(len(sys.argv) < 2):
+if(len(sys.argv) < 3):
     print("Not enough arguments!\n")
-    return
+    exit(0)
 
+dbfile = sys.argv[2] + ".db"
 rootdir = sys.argv[1]
 
 for root, dirs, files in os.walk(rootdir):
-    print files
+    if(dbfile in files):
+        dbpaths.append(os.path.join(root, dbfile))
+
+dbpaths.sort()
+
+print dbpaths
+
+for db in dbpaths:
+    con = sqlite3.connect(db)
+    c = con.cursor()
+
+    
+    c.execute("select count(domain) from WWW_{0} where ipv6 != \"n/a\"".format(sys.argv[2]))
+    count = c.fetchone()[0]
+
+    c.execute("select count(domain) from WWW_{0}".format(sys.argv[2]))
+    total = c.fetchone()[0]
+
+    print (float(count) / float(total))
